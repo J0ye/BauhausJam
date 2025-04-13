101c27662d32df55ca35bd58d13b9c9286ae910d
Martin <martin.fischer@fh-erfurt.de>
2025-04-13 19:31:21 +0200

    Finished Staring + Tutorial Screen

---
+++ Assets/Scripts/Musik.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Musik : MonoBehaviour
+{
+    public AudioMixer audioMixer; // Drag deinen Mixer hier rein im Inspector
+    public Slider volumeSlider;   // Dein UI-Slider
+    const string Mixer_Music = "Master";
+
+    /*
+    private void Awake()
+    {
+        volumeSlider.onValueChanged.AddListener(SetMusicVolume);
+    }
+
+    private void SetMusicVolume(float value)
+    {
+
+        value
+        audioMixer.SetFloat(Mixer_Music, value);
+    }*/
+
+    private void Start()
+    {
+        // Initialwert setzen
+        float volume;
+        audioMixer.GetFloat("Master", out volume);
+        volumeSlider.value = Mathf.Pow(10, volume / 20f); // Umwandlung von dB zu linear
+
+        // Listener hinzufügen
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+    }
+
+    public void SetVolume(float value)
+    {
+        // Lautstärke im Mixer setzen (linear → dB)
+        float volumeInDb = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat("Master", volumeInDb);
+    }
+}
+
